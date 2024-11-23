07b85f02542df0955619a07ed6357964b6231b35
EdgarMiranda20 <edgar.miranda.c20@gmail.com>
2024-11-22 23:18:25 -0600

    Add files via upload

---
+++ cicloWhile_IMC.java
@@ -0,0 +1,44 @@
+import javax.swing.JOptionPane;
+public class cicloWhile_IMC {
+    public static void main (String[]edg){
+        //Espacio para declaracion de variables
+        float peso,estatura,imc;
+         byte resp=1;//Inicializacion del while
+         while (resp<=1){
+           
+        peso=Float.parseFloat(JOptionPane.showInputDialog("Ingresa tu peso"));
+        estatura=Float.parseFloat(JOptionPane.showInputDialog("Ingresa tu estaura en formato de metros"));
+        imc=(peso/(estatura*estatura));
+        JOptionPane.showMessageDialog(null, "El indice de masa corporal es de: " + imc);                
+      
+        if(imc<18.49){
+            System.out.println("Peso Bajo");           
+        }
+        
+        else if(imc>=18.50 && imc<=24.99){
+            System.out.println("Peso normal");
+        }
+        
+        else if (imc>=25 && imc<=29.99) {
+            System.out.println("Sobrepeso");     
+        }
+        
+        else if (imc>=30 && imc<=34.99){
+            System.out.println("Obesidad grado I");
+        }
+        
+        else if(imc>=35 && imc<=39.99){
+            System.out.println("Obesidad grado II");
+        }
+        
+        else {
+            System.out.println("Obesidad Morbida");
+        }      
+         }
+        }
+    } //Cierra el metodo principal
+    
+
+
+    
+
