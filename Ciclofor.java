e5261c4efbab47d62aca8a945e8cacdba8e00bfe
EdgarMiranda20 <edgar.miranda.c20@gmail.com>
2024-12-11 07:06:26 -0600

    Update Ciclofor.java

--- Ciclofor.java
+++ Ciclofor.java
@@ -3,16 +3,17 @@ public class Ciclofor {
  public static void main (String[]edg){
         byte resp,num,x;
         int fact;
+  //Inicializacion del do-while
         do{
          
-        num=Byte.parseByte(JOptionPane.showInputDialog("Ingresa un nuemero"));
+        num=Byte.parseByte(JOptionPane.showInputDialog("Ingresa un nuemero: "));
         fact=1;
                 for(x=num; x>=1;x--){
                     fact*=x; 
                     System.out.println(fact + "\t");
                 }//cierra for
                 System.out.println("El factorial de " + num + " es: " + fact);
-                resp=Byte.parseByte(JOptionPane.showInputDialog("Presiona 1 para realizar" + "otro calculo"));
+                resp=Byte.parseByte(JOptionPane.showInputDialog("Presiona 1 si deseas realizar" + "otro calculo"));
                 
     
 }while(resp==1);
